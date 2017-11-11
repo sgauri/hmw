4daaf4bd46c84c9ef7450919a42e0702f26210d2
sgauri <sgauri61@gmail.com>
2017-11-12 03:41:02 +0530

    heroku versin

--- prime/settings.py
+++ prime/settings.py
@@ -1,21 +1,14 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+"""SECRET_KEY = '1hi3#n0)m@&=kwx@&zs!w3e4a^an-qcms1w+2c0_z288zk!2s6'"""
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1hi3#n0)m@&=kwx@&zs!w3e4a^an-qcms1w+2c0_z288zk!2s6')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1hi3#n0)m@&=kwx@&zs!w3e4a^an-qcms1w+2c0_z288zk!2s6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'emeros.apps.EmerosConfig',
@@ -57,10 +50,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'prime.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -68,9 +57,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,10 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Calcutta'
@@ -100,9 +82,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
